cde108e0961231875215ff0ee7b81b8a22040d44
LAPTOP-47DGQ5V8\Samuel Chu <chac3343@gmail.com>
2022-07-07 14:33:34 +0800

    seccond

---
+++ zoj_c356.cpp
@@ -0,0 +1,43 @@
+//艾加密小容量 
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int t;
+	cin>>t;
+	char a;
+	int r=0;//橫 
+	int c=0;//直 
+	int answer=0;//第幾個INDEX;
+	
+	while(cin>>a){ 
+		if(answer==t*r+c){
+		r++;
+		c++;
+		cout<<a;
+		}
+		answer++;
+	
+		if(answer==t*t)
+		break;
+	
+		
+		
+		
+		
+		
+	} 
+	
+		
+
+
+
+
+
+
+
+	return 0;
+}
+
